bece9c78b180c93e588ee0fa98f14f0d7575de45
michaelfehr <michael_fehrgmx.de>
2023-01-25 17:27:30 +0100

    Initial commit

---
+++ app/src/main/java/de/androidcrypto/googledriveplayground/MainActivity.java
@@ -0,0 +1,225 @@
+package de.androidcrypto.googledriveplayground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "GD Playground Main";
+
+    private final String basicFilename = "txtfile";
+    private final String fileExtension = ".txt";
+
+    Button generateFiles, signIn, queryFiles;
+    com.google.android.material.textfield.TextInputEditText fileName;
+
+
+    private DriveServiceHelper mDriveServiceHelper;
+    private static final int REQUEST_CODE_SIGN_IN = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateFiles = findViewById(R.id.btnMainGenerateFiles);
+        signIn = findViewById(R.id.btnMainSignIn);
+        queryFiles = findViewById(R.id.btnMainQueryFiles);
+        fileName = findViewById(R.id.etMainFilename);
+
+
+
+        queryFiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "query files on Google Drive");
+
+                if (mDriveServiceHelper == null) {
+                    Log.e(TAG, "ERROR on Querying for files");
+                    return;
+                }
+
+                    mDriveServiceHelper.queryFiles()
+                            .addOnSuccessListener(fileList -> {
+                                System.out.println("received filelist");
+                                System.out.println("list entries: " + fileList.getFiles().size());
+                                StringBuilder builder = new StringBuilder();
+                                for (File file : fileList.getFiles()) {
+                                    //builder.append(file.getName()).append("\n");
+                                    builder.append("fileName: " + file.getName() + " fileId:" + file.getId()).append("\n");
+                                    System.out.println("fileName: " + file.getName()
+                                            + " fileId: " + file.getId()
+                                    + " fileSize: " + file.getSize());
+
+                                }
+                                String fileNames = builder.toString();
+                                System.out.println(fileNames);
+                                fileName.setText(fileNames);
+                                //mDocContentEditText.setText(fileNames);
+
+                                //etReadOnlyMode();
+                            })
+                            .addOnFailureListener(exception -> Log.e(TAG, "Unable to query files.", exception));
+            }
+        });
+
+        generateFiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "generate files");
+                // this is generating 5 text files in internal storage
+                String basicString = "This is a test file for uploading to Google Drive.\nIt is file number ";
+
+                int numberOfFiles = 5;
+                for (int i = 1; i < numberOfFiles + 1; i++) {
+                    FileWriter writer = null;
+                    try {
+                        String filename = basicFilename + i + fileExtension;
+                        String dataToWrite = basicString + i + "\n" +
+                                "generated on " + new Date();
+                        java.io.File file = new java.io.File(view.getContext().getFilesDir(), filename);
+                        writer = new FileWriter(file);
+                        writer.append(dataToWrite);
+                        writer.flush();
+                        writer.close();
+                        Log.i(TAG, "file generated number: " + i);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Error: " + e.getMessage());
+                        Toast.makeText(MainActivity.this, "ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+                Toast.makeText(MainActivity.this, "generated " + numberOfFiles + " files in internal storage", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "sign in to Google Drive");
+                // Authenticate the user. For most apps, this should be done when the user performs an
+                // action that requires Drive access rather than in onCreate.
+                requestSignIn();
+            }
+        });
+    }
+
+    /**
+     * section sign-in to Google Drive account
+     */
+
+    /**
+     * Starts a sign-in activity using {@link #REQUEST_CODE_SIGN_IN}.
+     */
+    private void requestSignIn() {
+        Log.d(TAG, "Requesting sign-in");
+
+        // DriveScopes.DRIVE shows ALL files
+        // DriveScopes.DRIVE_FILE shows only files uploaded by this app
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .requestScopes(new Scope(DriveScopes.DRIVE))
+                        .build();
+/*
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                        .build();
+*/
+        GoogleSignInClient client = GoogleSignIn.getClient(this, signInOptions);
+
+        // The result of the sign-in Intent is handled in onActivityResult.
+        // todo handle deprecated startActivityForResult
+        startActivityForResult(client.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    /**
+     * Handles the {@code result} of a completed sign-in activity initiated from {@link
+     * #requestSignIn()}.
+     */
+    private void handleSignInResult(Intent result) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+                .addOnSuccessListener(googleAccount -> {
+                    Log.i(TAG, "Signed in as " + googleAccount.getEmail());
+                    Toast.makeText(MainActivity.this, "Signed in as " + googleAccount.getEmail(), Toast.LENGTH_SHORT).show();
+
+                    // Use the authenticated account to sign in to the Drive service.
+                    GoogleAccountCredential credential =
+                            GoogleAccountCredential.usingOAuth2(
+                                    this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                    credential.setSelectedAccount(googleAccount.getAccount());
+                    Drive googleDriveService =
+                            new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("Drive API Migration")
+                                    .build();
+
+                    // The DriveServiceHelper encapsulates all REST API and SAF functionality.
+                    // Its instantiation is required before handling any onClick actions.
+                    mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
+                })
+                .addOnFailureListener(exception -> {
+                    Log.e(TAG, "Unable to sign in.", exception);
+                    Toast.makeText(MainActivity.this, "Unable to sign in: " + exception.getMessage(), Toast.LENGTH_LONG).show();
+                });
+    }
+
+    /**
+     * section onActivityResult
+     * todo handle deprecated startActivityForResult
+     */
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        switch (requestCode) {
+            case REQUEST_CODE_SIGN_IN:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    handleSignInResult(resultData);
+                }
+                break;
+/*
+            case REQUEST_CODE_OPEN_DOCUMENT:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    Uri uri = resultData.getData();
+                    if (uri != null) {
+                        openFileFromFilePicker(uri);
+                    }
+                }
+                break;
+ */
+        }
+        super.onActivityResult(requestCode, resultCode, resultData);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,104 @@
+# Google Drive playground
+
+
+build.gradle (app):
+````plaintext
+
+    packagingOptions {
+        exclude 'META-INF/DEPENDENCIES'
+        exclude 'META-INF/LICENSE'
+        exclude 'META-INF/LICENSE.txt'
+        exclude 'META-INF/license.txt'
+        exclude 'META-INF/NOTICE'
+        exclude 'META-INF/NOTICE.txt'
+        exclude 'META-INF/notice.txt'
+        exclude 'META-INF/ASL2.0'
+    }
+
+
+    // google drive libs
+    //implementation 'com.google.android.gms:play-services-auth:17.0.0'
+    implementation 'com.google.android.gms:play-services-auth:20.4.1'
+    //implementation 'com.google.http-client:google-http-client-gson:1.26.0'
+    implementation 'com.google.http-client:google-http-client-gson:1.41.8'
+    implementation('com.google.api-client:google-api-client-android:1.26.0')
+    implementation('com.google.apis:google-api-services-drive:v3-rev136-1.25.0')
+    implementation 'com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava'
+````
+
+AndroidManifest.xml:
+```plaintext
+<uses-permission android:name="android.permission.INTERNET" />
+```
+
+For DriveServiceHelper.java see here:
+
+https://github.com/ammarptn/GDrive-Rest-Android/blob/master/gdriverest/src/main/java/com/ammarptn/gdriverest/DriveServiceHelper.java
+
+https://stackoverflow.com/questions/56949872/can-someone-provide-an-up-to-date-android-guide-for-google-drive-rest-api-v3
+
+https://stackoverflow.com/questions/69933562/android-deprecated-tasks-call-replacement
+
+https://developers.google.com/android/reference/com/google/android/gms/tasks/Tasks
+
+https://developers.google.com/android/reference/com/google/android/gms/tasks/TaskCompletionSource
+
+
+# Registering for Google Drive
+
+As we discuss we need to Google Drive SDK, So in order to use it, we need to enable that API. Okay don't worry we will move forward step by step
+
+- Go to [Google Console](https://console.developers.google.com/projectselector/apis/dashboard).
+- Sign up for a developer account if We don't have or then sign in.
+- `Create` a project Or `Select` and click continue from below,
+
+![Alt Text](https://thepracticaldev.s3.amazonaws.com/i/9tkmgg6z7s5fup0vxfxs.png)
+
+- After creating a project we have `Dashboard` like below,
+
+![Alt Text](https://thepracticaldev.s3.amazonaws.com/i/yd30f54tv84u4zbbqwup.png)
+
+
+- Now, Select `Library` on the left-hand side to go to the Search screen
+- Type “Google Drive” and select Google Drive API.
+- Select Enable. Then It looks like below,
+
+![Alt Text](https://thepracticaldev.s3.amazonaws.com/i/3jyyhw1qc16zdii50mye.png)
+
+- Back to Dashboard, from Dashboard left-hand side go to Credentials. Create `Credential` by selecting the `OAuth client ID` like below
+
+![Alt Text](https://thepracticaldev.s3.amazonaws.com/i/985j2fol9qo84nd04gln.png)
+
+Tt navigates to the `Create OAuth client ID` page. Select our Application Type `Android`.
+
+![Alt Text](https://thepracticaldev.s3.amazonaws.com/i/uc5b0l55srkulmptlo6x.png)
+
+- Now we need your `SHA-1` `Signing-certificate fingerprint` key. Do so follow below steps
+- Copy the keytool text (press the Copy icon) and paste it into a terminal.
+- Change the path-to-debug-or-production-keystore to your default debug keystore location:
+- On Mac or Linux, `~/.android/debug.keystore` .
+- On Windows, `%USERPROFILE%/.android/debug.keystore` .
+- After you execute the command, you will be prompted to enter the keystore password. The password for the debug keystore is blank by default, so you can just press Return or Enter.
+
+
+> Notes: If throwing error to generate singing key then it might be `keystore` not found.
+
+I used an alternative command for my machine `MAC OS`.
+
+```bash
+keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android
+```
+
+
+If everything works correctly, you should see something like this:
+- Copy the `SHA1` value from the terminal into the text field and press Create. The Client ID dialog will appear. Press OK.
+
+![Alt Text](https://thepracticaldev.s3.amazonaws.com/i/yeb93rnpvwwfspb7u6wc.png)
+
+- Finally, enter a name and the `package name` that we used to `create our app`. Although the hint refers to the package name in `AndroidManifest.xml`, it has to match the `applicationId` in `build.gradle` instead — otherwise, the login flow will fail.
+- We don't need to complete the form that appears, it's optional so press `Save` and move on.
+
+> Optional, Now, In the Credentials page. Authorization on Android uses the `SHA1` fingerprint and `package name` to `identify your app`, so you `don’t have to download` any `JSON file or copy any API key` or secret to our project.
+
+It about lots man, Don't worry! Now, are in the fun part 😎.
+
